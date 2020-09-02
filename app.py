70a7b675a006192fa70840db59595f6e75c44c2c
David Pratt <davidpratt512@gmail.com>
2020-09-01 19:44:27 -0700

    Add about page

--- app.py
+++ app.py
@@ -150,6 +150,11 @@ def redirect_from_short_url(short_url):
     return redirect(long_url, code=302)
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
